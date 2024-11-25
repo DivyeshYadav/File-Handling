15e0c80c6eda955defd387fe50b799b68ed50d47
DivyeshYadav <prabhakaryadav621@gmail.com>
2024-11-25 18:51:32 +0545

    FILE HANDLING

---
+++ main.py
@@ -0,0 +1,9 @@
+f1 = open("Text.txt","r")
+print(f1.read())
+
+f1 = open("Text.txt","w")
+print(f1.write("Hi my name is Divyesh Yadav"))
+
+
+f1 = open("Text.txt","a")
+print(f1.write(", and I am 9 years old"))
